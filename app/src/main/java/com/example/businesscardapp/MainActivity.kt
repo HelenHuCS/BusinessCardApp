e83bc5d549d2cc7a0e9b5bfb8b023c670a9e49f0
HelenHuCS <hailinghu1991@gmail.com>
2022-02-19 11:35:00 -0800

    Business Card.

---
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.businesscardapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.businesscardapp.R.layout.activity_main
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+    }
+}
